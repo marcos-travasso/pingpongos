9b6f7b4c65feea75965aa68910f41d15c4481f13
cetaphiil <cetaphil.git@gmail.com>
2023-05-05 16:27:28 -0300

    a

--- ppos_data.h
+++ ppos_data.h
@@ -8,67 +8,72 @@
 #define __PPOS_DATA__
 
 #include <stdio.h>
-#include <ucontext.h>		// biblioteca POSIX de trocas de contexto
-#include "queue.h"		// biblioteca de filas genéricas
+#include <ucontext.h> // biblioteca POSIX de trocas de contexto
+#include "queue.h"    // biblioteca de filas genéricas
 
 // Estrutura que define um Task Control Block (TCB)
 typedef struct task_t
 {
-   struct task_t *prev, *next ;		// ponteiros para usar em filas
-   int id ;				// identificador da tarefa
-   ucontext_t context ;			// contexto armazenado da tarefa
-   unsigned char state;  // indica o estado de uma tarefa (ver defines no final do arquivo ppos.h): 
-                          // n - nova, r - pronta, x - executando, s - suspensa, e - terminada
-   struct task_t* queue;
-   struct task_t* joinQueue;
-   int exitCode;
-   unsigned int awakeTime; // used to store the time when it should be waked up
+    struct task_t *prev, *next; // ponteiros para usar em filas
+    int id;                     // identificador da tarefa
+    ucontext_t context;         // contexto armazenado da tarefa
+    unsigned char state;        // indica o estado de uma tarefa (ver defines no final do arquivo ppos.h):
+                                // n - nova, r - pronta, x - executando, s - suspensa, e - terminada
+    struct task_t *queue;
+    struct task_t *joinQueue;
+    int exitCode;
+    unsigned int awakeTime; // used to store the time when it should be waked up
 
-   // ... (outros campos deve ser adicionados APOS esse comentario)
+    // ... (outros campos deve ser adicionados APOS esse comentario)
+
+    unsigned int timeFromStart;
 
     int staticPriority;
     int dynamicPriority;
 
-} task_t ;
+} task_t;
 
 // estrutura que define um semáforo
-typedef struct {
+typedef struct
+{
     struct task_t *queue;
     int value;
 
     unsigned char active;
-} semaphore_t ;
+} semaphore_t;
 
 // estrutura que define um mutex
-typedef struct {
+typedef struct
+{
     struct task_t *queue;
     unsigned char value;
 
     unsigned char active;
-} mutex_t ;
+} mutex_t;
 
 // estrutura que define uma barreira
-typedef struct {
+typedef struct
+{
     struct task_t *queue;
     int maxTasks;
     int countTasks;
     unsigned char active;
     mutex_t mutex;
-} barrier_t ;
+} barrier_t;
 
 // estrutura que define uma fila de mensagens
-typedef struct {
-    void* content;
+typedef struct
+{
+    void *content;
     int messageSize;
     int maxMessages;
     int countMessages;
-    
+
     semaphore_t sBuffer;
     semaphore_t sItem;
     semaphore_t sVaga;
-    
+
     unsigned char active;
-} mqueue_t ;
+} mqueue_t;
 
 #endif
-
