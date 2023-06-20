db4fa624652ba479527cc2a0123ea1e46f3ba629
cetaphiil <cetaphil.git@gmail.com>
2023-06-20 20:22:14 -0300

    Adição escalonadores e modificação Makefile.

--- Makefile
+++ Makefile
@@ -4,6 +4,12 @@ preemp: ppos-core-aux.o libppos_static.a pingpong-preempcao.o
 sche: ppos-core-aux.o libppos_static.a pingpong-scheduler.o
 			gcc ppos-core-aux.c libppos_static.a pingpong-scheduler.c -o sche
 
+disco1: ppos-core-aux.o libppos_static.a ppos_disk.o pingpong-disco1.o disk.o
+			gcc ppos-core-aux.c libppos_static.a ppos_disk.c pingpong-disco1.c disk.c -lrt
+
+disco2: ppos-core-aux.o libppos_static.a ppos_disk.o pingpong-disco2.o disk.o
+			gcc ppos-core-aux.c libppos_static.a ppos_disk.c pingpong-disco2.c disk.c -lrt
+
 ppos-core-aux.o: ppos-core-aux.c
 			gcc -c ppos-core-aux.c
 
@@ -13,8 +19,20 @@ pingpong-preempcao.o: pingpong-preempcao.c
 pingpong-scheduler.o: pingpong-scheduler.c
 			gcc -c pingpong-scheduler.c
 
-clean: *.o preemp sche
-			rm *.o	preemp sche
+disk.o: disk.c
+			gcc -c disk.c
+
+ppos_disk.o: ppos_disk.c
+			gcc -c ppos_disk.c
+
+pingpong-disco1.o: pingpong-disco1.c
+			gcc -c pingpong-disco1.c
+
+pingpong-disco2.o: pingpong-disco2.c
+			gcc -c pingpong-disco2.c
+
+clean: *.o preemp sche disco1 disco2
+			rm *.o	preemp sche disco1 disco2
 		
 target: dependencies
 	action
--- ppos_disk.c
+++ ppos_disk.c
@@ -4,40 +4,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
-//Se der algum problema de que a leitura dos blocos parece estar indo para o lado, troque o separador de linhas do disk.dat para LF
+// Se der algum problema de que a leitura dos blocos parece estar indo para o lado, troque o separador de linhas do disk.dat para LF
 struct sigaction usrsig;
-disk_t* disk;
+disk_t *disk;
 
-void add_waiting_task(waiting_task_t* task) {
+void add_waiting_task(waiting_task_t *task)
+{
     mutex_lock(&disk->queueMtx);
 
-    if(disk->waitingTasks == NULL) {
+    if (disk->waitingTasks == NULL)
+    {
         disk->waitingTasks = task;
         mutex_unlock(&disk->queueMtx);
         return;
     }
 
-    waiting_task_t* lt = disk->waitingTasks;
-    while(lt->next != NULL){
+    waiting_task_t *lt = disk->waitingTasks;
+    while (lt->next != NULL)
+    {
         lt = lt->next;
     }
 
     lt->next = task;
+
     mutex_unlock(&disk->queueMtx);
 }
 
-waiting_task_t* pop_waiting_task(){
-    if(disk->waitingTasks == NULL){
+waiting_task_t *pop_waiting_task()
+{
+    if (disk->waitingTasks == NULL)
+    {
         return NULL;
     }
-    waiting_task_t* wt = disk->waitingTasks;
+    waiting_task_t *wt = disk->waitingTasks;
     disk->waitingTasks = wt->next;
     wt->next = NULL;
     return wt;
 }
 
-waiting_task_t* create_waiting_task(task_t* task, int command, int block, void *buffer){
-    waiting_task_t* wt = malloc(sizeof(waiting_task_t));
+waiting_task_t *create_waiting_task(task_t *task, int command, int block, void *buffer)
+{
+    waiting_task_t *wt = malloc(sizeof(waiting_task_t));
     wt->task = task;
     wt->command = command;
     wt->block = block;
@@ -47,76 +54,194 @@ waiting_task_t* create_waiting_task(task_t* task, int command, int block, void *
     return wt;
 }
 
-int disk_execute_command(waiting_task_t* wt){
+int disk_execute_command(waiting_task_t *wt)
+{
     mutex_lock(&disk->diskAccessMtx);
 
     disk->processingTask = wt;
-    if(disk_cmd(wt->command, wt->block, wt->buffer) < 0) {
-        printf ("T%d - Erro escrevendo disco\n", task_id());
-        exit (1);
+    if (disk_cmd(wt->command, wt->block, wt->buffer) < 0)
+    {
+        printf("T%d - Erro escrevendo disco\n", task_id());
+        exit(1);
     }
 
     wt->task->state = PPOS_TASK_STATE_SUSPENDED;
     task_suspend(wt->task, &sleepQueue);
     task_yield();
 
-    while(disk_cmd(DISK_CMD_STATUS, 0, 0) != DISK_STATUS_IDLE){}
+    while (disk_cmd(DISK_CMD_STATUS, 0, 0) != DISK_STATUS_IDLE)
+    {
+    }
 
     mutex_unlock(&disk->diskAccessMtx);
 
     return 0;
 }
 
-void tratador (int signum){
+void tratador(int signum)
+{
     task_resume(disk->processingTask->task);
 }
 
-void disk_scheduler(void* arg){
-    while(sleepQueue != NULL || readyQueue != NULL){
-        waiting_task_t* wt = pop_waiting_task();
-        if(wt == NULL) {
+void disk_scheduler_FCFS(void *arg)
+{
+    int blocks_traveled = 0;
+    int currentblock = 0;
+    while (sleepQueue != NULL || readyQueue != NULL)
+    {
+        waiting_task_t *task = pop_waiting_task();
+        if (task != NULL)
+        {
+            int distance = task->block - currentblock;
+            if (distance < 0)
+                distance = currentblock - task->block;
+
+            blocks_traveled += distance;
+            currentblock = task->block;
+            task_resume(task->task);
+        }
+        else
+        {
             taskExec->state = PPOS_TASK_STATE_SUSPENDED;
             task_suspend(taskExec, &sleepQueue);
             task_yield();
-            continue;
         }
-        task_resume(wt->task);
     }
+    printf("Total de blocos percorridos: %d\n", blocks_traveled);
 }
 
-int disk_mgr_init (int *numBlocks, int *blockSize) {
+void disk_scheduler_CSCAN(void *arg)
+{
+    int currentblock = 0;
+    int diskSize = disk_cmd(DISK_CMD_DISKSIZE, 0, NULL);
+    int direction = 1;
+    int blocks_traveled = 0;
+
+    while (sleepQueue != NULL || readyQueue != NULL)
+    {
+        if (currentblock == (diskSize - 1) && direction == 1)
+        {
+            currentblock = 0;
+            blocks_traveled += diskSize - 1;
+            disk_cmd(DISK_CMD_READ, 0, NULL);
+            direction = -1;
+        }
+        waiting_task_t *task = pop_waiting_task();
+        if (task != NULL)
+        {
+            int distance;
+            if (task->block >= currentblock)
+            {
+                distance = task->block - currentblock;
+            }
+            else
+            {
+                distance = diskSize - currentblock + task->block;
+            }
+            blocks_traveled += distance;
+            currentblock = task->block;
+            task_resume(task->task);
+        }
+        else
+        {
+            taskExec->state = PPOS_TASK_STATE_SUSPENDED;
+            task_suspend(taskExec, &sleepQueue);
+            task_yield();
+        }
+    }
+    printf("Total de blocos percorridos: %d\n", blocks_traveled);
+}
+
+void disk_scheduler_SSTF(void *arg)
+{
+    int currentblock = 0;
+    int diskSize = disk_cmd(DISK_CMD_DISKSIZE, 0, NULL);
+    int blocks_traveled = 0;
+
+    while (sleepQueue != NULL || readyQueue != NULL)
+    {
+        waiting_task_t *selected_task = NULL;
+        int shortest_distance = 99999999;
+
+        waiting_task_t *task = disk->waitingTasks;
+
+        while (task != NULL)
+        {
+            int distance;
+
+            if (task->block >= currentblock)
+                distance = task->block - currentblock;
+            else
+                distance = currentblock - task->block;
+
+            if (distance < shortest_distance)
+            {
+                shortest_distance = distance;
+                selected_task = task;
+            }
+
+            task = task->next;
+        }
+
+        if (selected_task != NULL)
+        {
+            if (selected_task->block >= currentblock)
+                blocks_traveled += selected_task->block - currentblock;
+            else
+                blocks_traveled += currentblock - selected_task->block;
+            currentblock = selected_task->block;
+            task_resume(selected_task->task);
+            pop_waiting_task(selected_task);
+        }
+        else
+        {
+            taskExec->state = PPOS_TASK_STATE_SUSPENDED;
+            task_suspend(taskExec, &sleepQueue);
+            task_yield();
+        }
+    }
+    printf("Total de blocos percorridos: %d\n", blocks_traveled);
+}
+
+int disk_mgr_init(int *numBlocks, int *blockSize)
+{
     disk = malloc(sizeof(disk_t));
     disk->waitingTasks = NULL;
 
-    usrsig.sa_handler = tratador ;
-    sigemptyset (&usrsig.sa_mask) ;
-    usrsig.sa_flags = 0 ;
-    if (sigaction (SIGUSR1, &usrsig, 0) < 0) {
-        perror ("Erro em sigaction");
-        exit (1);
+    usrsig.sa_handler = tratador;
+    sigemptyset(&usrsig.sa_mask);
+    usrsig.sa_flags = 0;
+    if (sigaction(SIGUSR1, &usrsig, 0) < 0)
+    {
+        perror("Erro em sigaction");
+        exit(1);
     }
 
-    if (mutex_create(&disk->diskAccessMtx)) {
-        perror ("Erro criando mutex de acesso ao disco");
-        exit (1);
+    if (mutex_create(&disk->diskAccessMtx))
+    {
+        perror("Erro criando mutex de acesso ao disco");
+        exit(1);
     }
 
-    if (mutex_create(&disk->queueMtx)) {
-        perror ("Erro criando mutex de fila");
-        exit (1);
+    if (mutex_create(&disk->queueMtx))
+    {
+        perror("Erro criando mutex de fila");
+        exit(1);
     }
 
-    if(disk_cmd(DISK_CMD_INIT, 0, 0) < 0) {
-        perror ("Erro iniciando disco");
-        exit (1);
+    if (disk_cmd(DISK_CMD_INIT, 0, 0) < 0)
+    {
+        perror("Erro iniciando disco");
+        exit(1);
     }
 
     task_create(&disk->scheduler, disk_scheduler, "");
     return 0;
 }
 
-int disk_block_read (int block, void *buffer) {
-    waiting_task_t* wt = create_waiting_task(taskExec, DISK_CMD_READ, block, buffer);
+int disk_block_read(int block, void *buffer)
+{
+    waiting_task_t *wt = create_waiting_task(taskExec, DISK_CMD_READ, block, buffer);
 
     add_waiting_task(wt);
 
@@ -129,8 +254,9 @@ int disk_block_read (int block, void *buffer) {
     return 0;
 }
 
-int disk_block_write (int block, void *buffer) {
-    waiting_task_t* wt = create_waiting_task(taskExec, DISK_CMD_WRITE, block, buffer);
+int disk_block_write(int block, void *buffer)
+{
+    waiting_task_t *wt = create_waiting_task(taskExec, DISK_CMD_WRITE, block, buffer);
 
     add_waiting_task(wt);
 
@@ -142,4 +268,3 @@ int disk_block_write (int block, void *buffer) {
 
     return 0;
 }
-
